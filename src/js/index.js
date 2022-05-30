007d0253496c01d3c27655fdbc019c6766f5fc6f
victor conicy <akalazuvictor42@gmail.com>
2022-05-30 12:53:32 +0100

    new commit

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,3 @@ import { deFaultDogList } from "./defaultDoglis.js"
 import { btnRandom } from "./randomBtn.js"
 import { displayNumber } from "./displayNumber.js"
 import { dateString } from "./date.js"
-
-
-
-
-
-
-
-
-
-
-
-
-
